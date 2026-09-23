4067f6387e9b4647b42f5ab263d8c8901d78884c
radek <rajkonkret660@gmail.com>
2026-09-23 15:10:10 +0200

    [1 38] update streamlit_05

--- streamlit_05_zadanie/app.py
+++ streamlit_05_zadanie/app.py
@@ -1,24 +1,24 @@
 #Zbuduj panel kursanta.
-Sidebar:
-Kurs:
-Python
-Java
-SQL
-Główna część:
-Imię
-E-mail
-Poziom:
-początkujący
-średniozaawansowany
-zaawansowany
-Po przesłaniu formularza wyświetl:
-Dane kursanta
-
-Imię: ...
-E-mail: ...
-Kurs: ...
-Poziom: ...
-Wykorzystaj:
-st.sidebar
-st.form
-st.columns
+# Sidebar:
+# Kurs:
+# Python
+# Java
+# SQL
+# Główna część:
+# Imię
+# E-mail
+# Poziom:
+# początkujący
+# średniozaawansowany
+# zaawansowany
+# Po przesłaniu formularza wyświetl:
+# Dane kursanta
+#
+# Imię: ...
+# E-mail: ...
+# Kurs: ...
+# Poziom: ...
+# Wykorzystaj:
+# st.sidebar
+# st.form
+# st.columns
