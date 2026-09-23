03276e9ac73cd2fbbd7523e85333c3f584a18000
radek <rajkonkret660@gmail.com>
2026-09-23 13:46:35 +0200

    [1 25] init

---
+++ streamlit_01/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# (.venv) PS C:\Users\CSComarch\PycharmProjects\PythonProjectStreamLitApp> streamlit --version
+# Streamlit, version 1.64.0
+
+st.title("Moja pierwsza aplikacja Streamlit")
+
+st.write("Witaj w aplikacji napisanej w Pythonie")
