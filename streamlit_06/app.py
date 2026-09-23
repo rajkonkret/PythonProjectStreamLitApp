715ca14237eefb027bdeaf7b9dcd9e3d2a9f2d96
radek <rajkonkret660@gmail.com>
2026-09-23 15:57:03 +0200

    [1 43] update streamlit_06

--- streamlit_06/app.py
+++ streamlit_06/app.py
@@ -1,9 +1,27 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(
+    page_title="Analiza pliku CSV",
+    # layout="wide"
+)
 st.title("Analiza pliku CSV")
 
 uploaded_file = st.file_uploader(
     "Wybierz plik CSV",
     type=["csv"]
+)
+
+if uploaded_file is None:
+    st.info(
+        "Wgraj plik CSV."
+    )
+
+    st.stop()
+
+df = pd.read_csv(uploaded_file)
+
+st.dataframe(
+    df,
+    use_container_width=True
 )
