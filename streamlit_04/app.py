9c824bd72b46fb9ba203ef9d65a4d2aa5aa12ffb
radek <rajkonkret660@gmail.com>
2026-09-23 15:03:44 +0200

    [1 35] update streamlit_04

---
+++ streamlit_04/app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+st.title("Panel użytkownika")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header("Użytkownik")
+    st.write("Jan Kowalski")
+
+with col2:
+    st.header("Statystyki")
+    st.metric(
+        "Liczba kursów",
+        12
+    )
+
+left, right = st.columns([2, 1])
+
+with left:
+    st.write("Lewa Kolumna")
+
+with right:
+    st.write("Prawa kolumna")
+
+category  =st.sidebar.selectbox(
+    "Kategoria",
+    [
+        "Python",
+        "SQL",
+        "Python",
+    ]
+)
