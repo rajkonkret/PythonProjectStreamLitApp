fd46ea8ef29feda5257408b9e8cac275df8117e1
radek <rajkonkret660@gmail.com>
2026-09-23 14:23:43 +0200

    [1 30] update zad2

---
+++ streamlit_02/app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.title("Formularz użytkownika")
+
+name = st.text_input(
+    "Podaj swoje imię"
+)
+
+st.write("Wpisałeś:", name)
+
+age = st.number_input(
+    "Podaj wiek",
+    min_value=0,
+    max_value=130,
+    value=18
+)
+st.write("Twój wiek:", age)
+
+experience = st.slider(
+    "Ile lat programujesz?",
+    min_value=0,
+    max_value=30,
+    value=1
+)
+
+st.write("Lata:", experience)
