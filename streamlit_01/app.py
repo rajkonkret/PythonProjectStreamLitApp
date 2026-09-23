3761595b6e65bb631ca375aa6da2d4d6ce073b5d
radek <rajkonkret660@gmail.com>
2026-09-23 14:09:42 +0200

    [1 27] update zad1

--- streamlit_01/app.py
+++ streamlit_01/app.py
@@ -12,3 +12,37 @@ st.header("O Aplikacji")
 st.write(
     "Streamlit pozwala tworzyć aplikację webowe "
     "bez pisania HTML, CSS, JS.")
+
+st.write("Witaj")
+st.write("123")
+st.write(123)
+st.write(10 + 20)
+
+st.info("To jest informacja")
+st.success("Operacja zakończona sukcesem")
+st.error("Wystąpił błąd")
+st.warning("To jest ostrzeżenie")
+
+st.markdown("""
+### Informacje
+
+To jest **ważny tekst**.
+- Python
+- Streamlit
+- Pandas
+""")
+
+st.markdown("# Nagłówek H1")
+
+st.markdown(
+    '<p style="color:red;">Czerwony tekst</p>',
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    '<p style="color:red;">Czerwony tekst</p>',
+    unsafe_allow_html=False
+)
+
+st.markdown("---")
+st.markdown("[Kliknij](https://naukapythona.pl)")
