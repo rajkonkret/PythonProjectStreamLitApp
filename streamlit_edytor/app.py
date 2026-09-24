f42262ad74ec5575d2a35c9535c5a612cfe78d26
radek <rajkonkret660@gmail.com>
2026-09-24 11:28:20 +0200

    [2 36] update edytorn

--- streamlit_edytor/app.py
+++ streamlit_edytor/app.py
@@ -5,3 +5,15 @@ start = pd.DataFrame({
     "produkt": ["Notes", "Kubek"],
     "sztuki": [3, 2]
 })
+
+zmiany = st.data_editor(
+    start, num_rows="fixed", key="edycja"
+)
+
+if st.button("Sprawdź sumę"):
+    if zmiany["sztuki"].isna().any():
+        st.error("Uzupełnij liczbę sztuk.")
+    elif (zmiany['sztuki'] < 0).any():
+        st.error("Liczba nie może być ujemna.")
+    else:
+        st.write("Suma", zmiany['sztuki'].sum())
