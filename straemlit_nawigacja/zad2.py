d615fc768b08a93d60096d09d53f82fcb231239a
radek <rajkonkret660@gmail.com>
2026-09-24 10:50:36 +0200

    [2 28] update multipage

--- straemlit_nawigacja/zad2.py
+++ straemlit_nawigacja/zad2.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 
+
 def strona_glowna():
     st.title("Strona Głowna")
     st.write("Witaj w sklepie")
@@ -8,3 +9,15 @@ def strona_glowna():
     st.info("Boczny panel - menu")
     st.metric("Licznik odwiedzin:", st.session_state.licznik_odwiedzin)
 
+
+def strona_raport():
+    st.title("Raport")
+    st.write("Podsumowanie")
+
+    dane = pd.DataFrame({
+        "Produkt": ["Klawiatura", "Mysz", "Monitor"],
+        "SPrzedaż": [1200, 450, 2100]
+    })
+
+    st.dataframe(dane, hide_index=True)
+    st.bar_chart(dane.set_index("Produkt"))
