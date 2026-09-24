842a278033fdf7e4960761a5d79c385386ede4a0
radek <rajkonkret660@gmail.com>
2026-09-24 09:06:16 +0200

    [2 12] update streamlit_10

---
+++ streamlit_10/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import time
+import pandas as pd
+import numpy as np
+
+st.set_page_config(page_title="Streamlit: Istota Cache", layout="wide")
+
+st.title("@st.cache_data")
+
+st.divider()
