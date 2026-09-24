307c2cf4b280c82f79ef314f5678ee08ca132d92
radek <rajkonkret660@gmail.com>
2026-09-24 09:13:07 +0200

    [2 14] update streamlit_10

--- streamlit_10/app.py
+++ streamlit_10/app.py
@@ -8,3 +8,14 @@ st.set_page_config(page_title="Streamlit: Istota Cache", layout="wide")
 st.title("@st.cache_data")
 
 st.divider()
+
+def pobierz_dane_bez_cache(kategoria: str):
+    time.sleep(2)
+    np.random.seed(42)
+    df = pd.DataFrame(
+        {
+            "Produkt": [f"{kategoria} #{i}" for i in range(1, 6)],
+            "Cena": np.random.randint(50, 500, size=5),
+            "Pobrano_o": time.strftime("%H:%M:%S")
+        }
+    )
